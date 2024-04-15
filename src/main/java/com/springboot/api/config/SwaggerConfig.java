d1096dec13970e630c98a1c104881f7d2aae0c2a
wjdsheep15 <wjd15sheep@gmail.com>
2024-04-15 10:59:08 +0900

    Fix change

--- src/main/java/com/springboot/api/config/SwaggerConfig.java
+++ src/main/java/com/springboot/api/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+
 @Configuration
 @EnableWebMvc
 public class SwaggerConfig {
